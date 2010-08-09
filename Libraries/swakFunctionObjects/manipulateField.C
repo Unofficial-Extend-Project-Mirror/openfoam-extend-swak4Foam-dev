fbbdd976170a369dab997b467c00902f8db47d09
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2010-08-09 23:47:33 +0200

    Manipuliert jetzt Felder

---
+++ Libraries/swakFunctionObjects/manipulateField.C
@@ -0,0 +1,149 @@
+//  OF-extend Revision: $Id$ 
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright  held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "manipulateField.H"
+
+#include "FieldValueExpressionDriver.H"
+
+namespace Foam {
+    defineTypeNameAndDebug(manipulateField,0);
+}
+
+Foam::manipulateField::manipulateField
+(
+    const word& name,
+    const objectRegistry& obr,
+    const dictionary& dict,
+    const bool loadFromFiles
+)
+:
+    active_(true),
+    obr_(obr)
+{
+    if (!isA<fvMesh>(obr_))
+    {
+        active_=false;
+        WarningIn("manipulateField::manipulateField")
+            << "Not a fvMesh. Nothing I can do"
+                << endl;
+    }
+    read(dict);
+    execute();
+}
+
+Foam::manipulateField::~manipulateField()
+{}
+
+template<class T>
+void Foam::manipulateField::manipulate(
+    const T *data,
+    const volScalarField *mask
+)
+{
+    T &original=const_cast<T &>(obr_.lookupObject<T>(name_));
+
+    forAll(original,cellI) {
+        if((*mask)[cellI]>SMALL) {
+            original[cellI]=(*data)[cellI];
+        }
+    }
+    original.correctBoundaryConditions();
+}
+
+void Foam::manipulateField::read(const dictionary& dict)
+{
+    if(active_) {
+        name_=word(dict.lookup("fieldName"));
+        expression_=string(dict.lookup("expression"));
+        maskExpression_=string(dict.lookup("mask"));
+    }
+}
+
+void Foam::manipulateField::execute()
+{
+    if(active_) {
+        const fvMesh& mesh = refCast<const fvMesh>(obr_);
+        
+        FieldValueExpressionDriver driver(
+            mesh.time().timeName(),
+            mesh.time(),
+            mesh,
+            false, // no caching. No need
+            true,  // search fields in memory
+            false  // don't look up files in memory
+        );
+
+        driver.parse(expression_);
+
+        FieldValueExpressionDriver ldriver(
+            mesh.time().timeName(),
+            mesh.time(),
+            mesh,
+            false, // no caching. No need
+            true,  // search fields in memory
+            false  // don't look up files in memory
+        );
+
+        ldriver.parse(maskExpression_);
+
+        if(!ldriver.resultIsLogical()) {
+            FatalErrorIn("manipulateField::execute()")
+                << maskExpression_ << " does not evaluate to a logical expression"
+                    << endl
+                    << abort(FatalError);
+        }
+
+        if(driver.resultIsVector()) {
+            manipulate(
+                driver.getVector(),
+                ldriver.getScalar()
+            );
+            
+        } else if(driver.resultIsScalar()) {
+            manipulate(
+                driver.getScalar(),
+                ldriver.getScalar()
+            );
+        } else {
+            WarningIn("Foam::manipulateField::execute()")
+                << "Expression '" << expression_ 
+                    << "' evaluated to an unsupported type"
+                    << endl;
+        }
+    }
+}
+
+
+void Foam::manipulateField::end()
+{
+}
+
+void Foam::manipulateField::write()
+{
+}
+
+// ************************************************************************* //
