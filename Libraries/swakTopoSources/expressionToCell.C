f456f36a35fcf01df155a8199a0c313fd0f6640d
Bernhard Gschaider <bgschaid@ice-sf.at>
2010-08-31 15:29:15 +0200

    Einfaches Kopieren 2er Originalklassen

---
+++ Libraries/swakTopoSources/expressionToCell.C
@@ -0,0 +1,129 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "expressionToCell.H"
+#include "polyMesh.H"
+#include "cellSet.H"
+
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+defineTypeNameAndDebug(expressionToCell, 0);
+
+addToRunTimeSelectionTable(topoSetSource, expressionToCell, word);
+
+addToRunTimeSelectionTable(topoSetSource, expressionToCell, istream);
+
+}
+
+
+Foam::topoSetSource::addToUsageTable Foam::expressionToCell::usage_
+(
+    expressionToCell::typeName,
+    "\n    Usage: expressionToCell <cellSet>\n\n"
+    "    Select all cells in the cellSet\n\n"
+);
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+// Construct from components
+Foam::expressionToCell::expressionToCell
+(
+    const polyMesh& mesh,
+    const word& setName
+)
+:
+    topoSetSource(mesh),
+    setName_(setName)
+{}
+
+
+// Construct from dictionary
+Foam::expressionToCell::expressionToCell
+(
+    const polyMesh& mesh,
+    const dictionary& dict
+)
+:
+    topoSetSource(mesh),
+    setName_(dict.lookup("set"))
+{}
+
+
+// Construct from Istream
+Foam::expressionToCell::expressionToCell
+(
+    const polyMesh& mesh,
+    Istream& is
+)
+:
+    topoSetSource(mesh),
+    setName_(checkIs(is))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::expressionToCell::~expressionToCell()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::expressionToCell::applyToSet
+(
+    const topoSetSource::setAction action,
+    topoSet& set
+) const
+{
+    if ((action == topoSetSource::ADD) || (action == topoSetSource::NEW))
+    {
+        Info<< "    Adding all elements of cellSet " << setName_ << " ..."
+            << endl;
+
+        // Load the set
+        cellSet loadedSet(mesh_, setName_);
+
+        set.addSet(loadedSet);
+    }
+    else if (action == topoSetSource::DELETE)
+    {
+        Info<< "    Removing all elements of cellSet " << setName_ << " ..."
+            << endl;
+
+        // Load the set
+        cellSet loadedSet(mesh_, setName_);
+
+        set.deleteSet(loadedSet);
+    }
+}
+
+
+// ************************************************************************* //
---
+++ Libraries/swakTopoSources/expressionToFace.C
@@ -0,0 +1,234 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "expressionToFace.H"
+#include "polyMesh.H"
+#include "cellSet.H"
+#include "Time.H"
+#include "syncTools.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+defineTypeNameAndDebug(expressionToFace, 0);
+
+addToRunTimeSelectionTable(topoSetSource, expressionToFace, word);
+
+addToRunTimeSelectionTable(topoSetSource, expressionToFace, istream);
+
+}
+
+
+Foam::topoSetSource::addToUsageTable Foam::expressionToFace::usage_
+(
+    expressionToFace::typeName,
+    "\n    Usage: expressionToFace <cellSet> all|both\n\n"
+    "    Select -all : all faces of cells in the cellSet\n"
+    "           -both: faces where both neighbours are in the cellSet\n\n"
+);
+
+template<>
+const char* Foam::NamedEnum<Foam::expressionToFace::cellAction, 2>::names[] =
+{
+    "all",
+    "both"
+};
+
+const Foam::NamedEnum<Foam::expressionToFace::cellAction, 2>
+    Foam::expressionToFace::cellActionNames_;
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::expressionToFace::combine(topoSet& set, const bool add) const
+{
+    // Load the set
+    if (!exists(mesh_.time().path()/topoSet::localPath(mesh_, setName_)))
+    {
+        SeriousError<< "Cannot load set "
+            << setName_ << endl;
+    }
+
+    cellSet loadedSet(mesh_, setName_);
+
+    if (option_ == ALL)
+    {
+        // Add all faces from cell
+        for
+        (
+            cellSet::const_iterator iter = loadedSet.begin();
+            iter != loadedSet.end();
+            ++iter
+        )
+        {
+            label cellI = iter.key();
+
+            const labelList& cFaces = mesh_.cells()[cellI];
+
+            forAll(cFaces, cFaceI)
+            {
+                addOrDelete(set, cFaces[cFaceI], add);
+            }
+        }
+    }
+    else if (option_ == BOTH)
+    {
+        // Add all faces whose both neighbours are in set.
+
+        label nInt = mesh_.nInternalFaces();
+        const labelList& own = mesh_.faceOwner();
+        const labelList& nei = mesh_.faceNeighbour();
+        const polyBoundaryMesh& patches = mesh_.boundaryMesh();
+
+
+        // Check all internal faces
+        for (label faceI = 0; faceI < nInt; faceI++)
+        {
+            if (loadedSet.found(own[faceI]) && loadedSet.found(nei[faceI]))
+            {
+                addOrDelete(set, faceI, add);
+            }
+        }
+
+
+        // Get coupled cell status
+        boolList neiInSet(mesh_.nFaces()-nInt, false);
+
+        forAll(patches, patchI)
+        {
+            const polyPatch& pp = patches[patchI];
+
+            if (pp.coupled())
+            {
+                label faceI = pp.start();
+                forAll(pp, i)
+                {
+                    neiInSet[faceI-nInt] = loadedSet.found(own[faceI]);
+                    faceI++;
+                }
+            }
+        }
+        syncTools::swapBoundaryFaceList(mesh_, neiInSet, false);
+
+
+        // Check all boundary faces
+        forAll(patches, patchI)
+        {
+            const polyPatch& pp = patches[patchI];
+
+            if (pp.coupled())
+            {
+                label faceI = pp.start();
+                forAll(pp, i)
+                {
+                    if (loadedSet.found(own[faceI]) && neiInSet[faceI-nInt])
+                    {
+                        addOrDelete(set, faceI, add);
+                    }
+                    faceI++;
+                }
+            }
+        }
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+// Construct from componenta
+Foam::expressionToFace::expressionToFace
+(
+    const polyMesh& mesh,
+    const word& setName,
+    const cellAction option
+)
+:
+    topoSetSource(mesh),
+    setName_(setName),
+    option_(option)
+{}
+
+
+// Construct from dictionary
+Foam::expressionToFace::expressionToFace
+(
+    const polyMesh& mesh,
+    const dictionary& dict
+)
+:
+    topoSetSource(mesh),
+    setName_(dict.lookup("set")),
+    option_(cellActionNames_.read(dict.lookup("option")))
+{}
+
+
+// Construct from Istream
+Foam::expressionToFace::expressionToFace
+(
+    const polyMesh& mesh,
+    Istream& is
+)
+:
+    topoSetSource(mesh),
+    setName_(checkIs(is)),
+    option_(cellActionNames_.read(checkIs(is)))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::expressionToFace::~expressionToFace()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::expressionToFace::applyToSet
+(
+    const topoSetSource::setAction action,
+    topoSet& set
+) const
+{
+    if ((action == topoSetSource::NEW) || (action == topoSetSource::ADD))
+    {
+        Info<< "    Adding faces according to cellSet " << setName_
+            << " ..." << endl;
+
+        combine(set, true);
+    }
+    else if (action == topoSetSource::DELETE)
+    {
+        Info<< "    Removing faces according to cellSet " << setName_
+            << " ..." << endl;
+
+        combine(set, false);
+    }
+}
+
+
+// ************************************************************************* //
