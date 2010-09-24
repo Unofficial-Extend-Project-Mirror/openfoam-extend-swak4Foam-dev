996365f490fb0df47a222ac5a92c23c008b23695
Bernhard Gschaider <bgschaid@ice-sf.at>
2010-09-24 15:54:38 +0200

    Preparing for parallel running

---
+++ Examples/other/angledDuctImplicit/decomposeMesh.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+./makeMesh.sh
+
+pyFoamDecompose.py --clear . $1
+
+cp system/cellSetDict.3 system/cellSetDict
+pyFoamRunner.py --proc=$1 cellSet
+cp system/cellSetDict.4 system/cellSetDict
+pyFoamRunner.py --proc=$1 cellSet
+
+cp system/faceSetDict.3 system/faceSetDict
+pyFoamRunner.py --proc=$1 faceSet
+cp system/faceSetDict.4 system/faceSetDict
+pyFoamRunner.py --proc=$1 faceSet
+
+
