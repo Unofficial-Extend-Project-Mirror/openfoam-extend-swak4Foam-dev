7f19a0df9e1936329ddab0b3f88ed908b94a3902
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2010-09-24 17:35:31 +0200

    Now the case works in parallel

--- Examples/other/angledDuctImplicit/decomposeMesh.sh
+++ Examples/other/angledDuctImplicit/decomposeMesh.sh
@@ -8,6 +8,8 @@ cp system/cellSetDict.3 system/cellSetDict
 pyFoamRunner.py --proc=$1 cellSet
 cp system/cellSetDict.4 system/cellSetDict
 pyFoamRunner.py --proc=$1 cellSet
+cp system/cellSetDict.5 system/cellSetDict
+pyFoamRunner.py --proc=$1 cellSet
 
 cp system/faceSetDict.3 system/faceSetDict
 pyFoamRunner.py --proc=$1 faceSet
