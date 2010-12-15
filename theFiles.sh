74fe0d48a69656a2683e51b9132a6f35c182e063
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2010-12-15 11:26:22 +0100

    Added scripts to make libraries and utilities globally available

---
+++ theFiles.sh
@@ -0,0 +1,9 @@
+export SWAKLIBS=(groovyBC \
+    simpleSwakFunctionObjects \
+    swakTopoSources \
+    swak4FoamParsers \
+    swakFunctionObjects)
+
+export SWAKUTILS=(funkySetBoundaryField \
+    funkySetFields \
+    replayTransientBC)
