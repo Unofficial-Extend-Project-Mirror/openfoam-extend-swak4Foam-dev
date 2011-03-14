597d01f71be07a49d0c9d508853aab911b995307
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2011-03-14 23:31:09 +0100

    Framework is there but no calculations are done yet

---
+++ Utilities/funkyDoCalc/funkyDoCalc.C
@@ -0,0 +1,97 @@
+/*---------------------------------------------------------------------------*\
+ ##   ####  ######     | 
+ ##  ##     ##         | Copyright: ICE Stroemungsfoschungs GmbH
+ ##  ##     ####       |
+ ##  ##     ##         | http://www.ice-sf.at
+ ##   ####  ######     |
+-------------------------------------------------------------------------------
+ =========                   |
+ \\      /   F ield          | OpenFOAM: The Open Source CFD Toolbox
+  \\    /    O peration      |
+   \\  /     A nd            | Copyright (C) 1991-2005 OpenCFD Ltd.
+    \\/      M anipulation   |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    funkyDoCalc
+
+Description
+
+ ICE Revision: $Id$ 
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+#include "CommonValueExpressionDriver.H"
+
+#include "timeSelector.H"
+
+#include "IFstream.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// Main program:
+
+int main(int argc, char *argv[])
+{
+
+    Foam::timeSelector::addOptions(false);
+    Foam::argList::validArgs.append("expressionDict");
+#   include "addRegionOption.H"
+
+#   include "setRootCase.H"
+
+   IFstream theFile(args.args()[1]);
+   dictionary theExpressions(theFile);
+   
+    if (!args.options().found("time") && !args.options().found("latestTime")) {
+        FatalErrorIn("main()")
+            << args.executable()
+                << ": time/latestTime option is required" << endl
+            << exit(FatalError);
+    }
+
+#   include "createTime.H"
+    Foam::instantList timeDirs = Foam::timeSelector::select0(runTime, args);
+
+#   include "createNamedMesh.H"
+
+    forAll(timeDirs, timeI)
+    {
+        runTime.setTime(timeDirs[timeI], timeI);
+
+        Foam::Info<< "Time = " << runTime.timeName() << Foam::endl;
+
+        mesh.readUpdate();
+       
+       forAllConstIter(IDLList<entry>, theExpressions, iter) 
+       {
+	  Info << iter().keyword() << ":" << endl;
+
+          autoPtr<CommonValueExpressionDriver> driver=
+              CommonValueExpressionDriver::New(iter().dict(),mesh); 
+       }
+    }
+    
+    Info << "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
