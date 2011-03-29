ff3c4b6c05d1e65bbba97f960fc66e6cd88c1585
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2011-03-29 16:32:31 +0200

    Simple script to compare the current results to the old results

---
+++ Examples/other/angledDuctImplicit/compareToBaseline.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+# Compare the current data with the data in the baselines
+# Changes have to be detected manually and are either due to
+# a) a change in OpenFOAM
+# b) a bug in swak4Foam
+
+import sys
+if len(sys.argv)>1:
+    vectorMode=sys.argv[1]
+else:
+    vectorMode="x"
+    
+from os import listdir,path
+from PyFoam.Applications.TimelinePlot import TimelinePlot
+
+for f in listdir("baselineData"):
+    print "\n\n Comparing:",f
+    TimelinePlot(args=[".",
+                       "--dir="+f,
+                       "--reference-dir="+path.join("baselineData",f),
+                       "--basic-mode=lines",
+                       "--vector-mode="+vectorMode,
+                       "--compare","--metrics"])
