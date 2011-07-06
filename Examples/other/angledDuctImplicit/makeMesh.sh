6ef69da78078552e256a785013b23c5bcdfae0e2
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2011-07-06 22:20:03 +0200

    All the example cases (excluding those that need a olver of their own) now run. Some fail after some time. Some give different results compared with the baseline (which may be due to the different solution algorithms)

--- Examples/other/angledDuctImplicit/makeMesh.sh
+++ Examples/other/angledDuctImplicit/makeMesh.sh
@@ -1,15 +1,10 @@
 #! /bin/sh
- 
+
+rm -f constant/polyMesh/*.gz
+rm -rf constant/polyMesh/sets
+
 blockMesh
 
-cp system/faceSetDict.1 system/faceSetDict
-faceSet
-cp system/faceSetDict.2 system/faceSetDict
-faceSet
-
-cp system/cellSetDict.1 system/cellSetDict
-cellSet
-cp system/cellSetDict.2 system/cellSetDict
-cellSet
+topoSet
 
 setsToZones
