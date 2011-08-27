411f18438b4a28e7f88e3248714945f867e94233
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2011-08-27 15:36:21 +0200

    Reasonable value for the reversion point

--- Examples/PythonIntegration/findPointPitzDaily/executeFindPoint.py
+++ Examples/PythonIntegration/findPointPitzDaily/executeFindPoint.py
@@ -2,12 +2,14 @@
 Uinternal=OpenFOAM.vectorField(Upatch.patchInternalField())
 xOld=xMin
 xRevert=xMax
+UOld=Uinternal[faceIDs[0]].x()
+
 for i in faceIDs:
-    if Uinternal[i].x()>0:
+    if Uinternal[i].x()>0 and UOld<=0:
         xRevert=0.5*(xOld+Upatch.patch().Cf()[i].x())
-        break
     else:
         xOld=Upatch.patch().Cf()[i].x()
+    UOld=Uinternal[i].x()
 
 print "Ux changed direction at",xRevert
 
