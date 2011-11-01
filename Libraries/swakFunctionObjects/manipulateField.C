bb1d3ac2a3b37844e3f167ba0a9fa1cb271c9228
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2011-11-01 22:55:09 +0100

    Fixed problem with broken function objects (rereading)

--- Libraries/swakFunctionObjects/manipulateField.C
+++ Libraries/swakFunctionObjects/manipulateField.C
@@ -45,8 +45,6 @@ Foam::manipulateField::manipulateField
     obr_(obr),
     dict_(dict)
 {
-    driver_->createWriterAndRead(name+"_"+type());
-
     if (!isA<fvMesh>(obr_))
     {
         active_=false;
