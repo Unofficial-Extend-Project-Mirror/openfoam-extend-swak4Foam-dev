4706df6caafab5eac049e3867de8e992342d9747
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2012-06-05 20:22:30 +0200

    Reproduce the problem

--- Examples/other/angledDuctImplicit/makeMesh.sh
+++ Examples/other/angledDuctImplicit/makeMesh.sh
@@ -6,6 +6,10 @@ cp system/faceSetDict.1 system/faceSetDict
 faceSet
 cp system/faceSetDict.2 system/faceSetDict
 faceSet
+cp system/faceSetDict.5 system/faceSetDict
+faceSet
+cp system/faceSetDict.6 system/faceSetDict
+faceSet
 
 cp system/cellSetDict.1 system/cellSetDict
 cellSet
