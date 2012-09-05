f5dd194fbcd503b85174758a9fadfd04804f726b
Bernhard Gschaider <bgschaid@ice-sf.at>
2012-09-05 14:59:07 +0200

    A simple functionObject that ends the run if a limit is exceeded

---
+++ Libraries/simpleFunctionObjects/misc/writeAndEndFieldRangeFunctionObject/writeAndEndFieldRangeFunctionObject.C
@@ -0,0 +1,101 @@
+//  OF-extend Revision: $Id$
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright  held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "writeAndEndFieldRangeFunctionObject.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "polyMesh.H"
+#include "IOmanip.H"
+#include "Time.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(writeAndEndFieldRangeFunctionObject, 0);
+
+    addToRunTimeSelectionTable
+    (
+        functionObject,
+        writeAndEndFieldRangeFunctionObject,
+        dictionary
+    );
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+writeAndEndFieldRangeFunctionObject::writeAndEndFieldRangeFunctionObject
+(
+    const word &name,
+    const Time& t,
+    const dictionary& dict
+)
+:
+    writeAndEndFunctionObject(name,t,dict),
+    fieldName_(""),
+    maximum_(HUGE),
+    minimum_(-HUGE)
+{
+}
+
+bool writeAndEndFieldRangeFunctionObject::start()
+{
+    writeAndEndFunctionObject::start();
+
+    fieldName_=word(dict_.lookup("fieldName"));
+    minimum_=readScalar(dict_.lookup("minimum"));
+    maximum_=readScalar(dict_.lookup("maximum"));
+
+    Info << "Checking for field " << fieldName_ << " in range [ " << minimum_
+        << " , " << maximum_ << " ] " << endl;
+
+    return true;
+}
+
+bool writeAndEndFieldRangeFunctionObject::endRunNow()
+{
+    if(
+        check<volScalarField>()
+        ||
+        check<volVectorField>()
+        ||
+        check<volSphericalTensorField>()
+        ||
+        check<volSymmTensorField>()
+        ||
+        check<volTensorField>()
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+} // namespace Foam
+
+// ************************************************************************* //
---
+++ Libraries/simpleFunctionObjects/misc/writeAndEndFunctionObject/writeAndEndFunctionObject.C
@@ -0,0 +1,73 @@
+//  OF-extend Revision: $Id$
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright  held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "writeAndEndFunctionObject.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "polyMesh.H"
+#include "IOmanip.H"
+#include "Time.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(writeAndEndFunctionObject, 0);
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+writeAndEndFunctionObject::writeAndEndFunctionObject
+(
+    const word &name,
+    const Time& t,
+    const dictionary& dict
+)
+:
+    simpleFunctionObject(name,t,dict)
+{
+}
+
+bool writeAndEndFunctionObject::start()
+{
+    simpleFunctionObject::start();
+
+    return true;
+}
+
+void writeAndEndFunctionObject::write()
+{
+    if(this->endRunNow()) {
+        Info << "Ending run because of functionObject " << this->name() << endl;
+        const_cast<Time &>(time()).writeAndEnd();
+    }
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+} // namespace Foam
+
+// ************************************************************************* //
