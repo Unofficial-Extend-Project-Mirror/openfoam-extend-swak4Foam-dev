e784ff3b10482b0cf1a0b96878031a16ddc8e99e
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2012-09-11 00:17:50 +0200

    A pluginfunction to calculate the source by a cloud. Because of the templatization this only works for one specific class

---
+++ Libraries/functionPlugins/swakLagrangianCloudSourcesFunctionPlugin/lcsMomentumSourcePluginFunction.C
@@ -0,0 +1,91 @@
+/*---------------------------------------------------------------------------*\
+ ##   ####  ######     |
+ ##  ##     ##         | Copyright: ICE Stroemungsfoschungs GmbH
+ ##  ##     ####       |
+ ##  ##     ##         | http://www.ice-sf.at
+ ##   ####  ######     |
+-------------------------------------------------------------------------------
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2008 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+ ICE Revision: $Id$
+\*---------------------------------------------------------------------------*/
+
+#include "lcsMomentumSourcePluginFunction.H"
+
+#include "addToRunTimeSelectionTable.H"
+
+namespace Foam {
+
+defineTypeNameAndDebug(lcsMomentumSourcePluginFunction,0);
+addNamedToRunTimeSelectionTable(FieldValuePluginFunction,lcsMomentumSourcePluginFunction , name, lcsMomentumSource);
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+lcsMomentumSourcePluginFunction::lcsMomentumSourcePluginFunction(
+    const FieldValueExpressionDriver &parentDriver,
+    const word &name
+):
+    LagrangianCloudSourcePluginFunction<basicKinematicCloud,kinematicCloud>(
+        parentDriver,
+        name,
+        "volVectorField"
+    )
+{
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void lcsMomentumSourcePluginFunction::doEvaluation()
+{
+    DimensionedField< vector, volMesh > SU(cloud().SU());
+
+    autoPtr<volVectorField> pSource(
+        new volVectorField(
+            IOobject(
+                cloudName()+"MomentumSource",
+                mesh().time().timeName(),
+                mesh(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            mesh(),
+            SU.dimensions(),
+            "zeroGradient"
+        )
+    );
+
+    pSource->internalField()=SU.field();
+
+    result().setObjectResult(pSource);
+}
+
+// * * * * * * * * * * * * * * * Concrete implementations * * * * * * * * * //
+
+
+} // namespace
+
+// ************************************************************************* //
