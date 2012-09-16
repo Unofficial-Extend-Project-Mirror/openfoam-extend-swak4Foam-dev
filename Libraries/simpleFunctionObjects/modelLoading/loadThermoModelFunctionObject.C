33ef2052795354dd887e5756a68601ddbd6c834e
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2012-09-16 19:28:19 +0200

    Added function objects that can create a thermophysical model

---
+++ Libraries/simpleFunctionObjects/modelLoading/loadThermoModelFunctionObject.C
@@ -0,0 +1,68 @@
+//  OF-extend Revision: $Id$
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright  held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "loadThermoModelFunctionObject.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "fvMesh.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template <class ModelType>
+loadThermoModelFunctionObject<ModelType>::loadThermoModelFunctionObject
+(
+    const word& name,
+    const Time& t,
+    const dictionary& dict
+)
+:
+    modelLoadingFunctionObject<ModelType>(name,t,dict)
+{
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template <class ModelType>
+autoPtr<ModelType> loadThermoModelFunctionObject<ModelType>::initModel()
+{
+    return ModelType::New(
+        dynamicCast<const fvMesh &>(
+            this->obr()
+        )
+    );
+}
+
+
+} // namespace Foam
+
+// ************************************************************************* //
