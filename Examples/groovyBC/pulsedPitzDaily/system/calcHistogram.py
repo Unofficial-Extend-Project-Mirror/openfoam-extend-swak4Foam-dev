901ba398b42e1857ba061e188ddaf2f480a201d3
Bernhard Gschaider <bgschaid@ice-sf.at>
2012-12-13 11:25:22 +0100

    fix problem with older numpy-versions

--- Examples/groovyBC/pulsedPitzDaily/system/calcHistogram.py
+++ Examples/groovyBC/pulsedPitzDaily/system/calcHistogram.py
@@ -1,4 +1,9 @@
-vals,edges=numpy.histogram(a=UField.x,bins=50,weights=volume,density=True)
+try:
+    vals,edges=numpy.histogram(a=UField.x,bins=50,weights=volume,density=True)
+except TypeError:
+    # old numpy-versions don't have the density parameter
+    vals,edges=numpy.histogram(a=UField.x,bins=50,weights=volume)
+
 xVals=0.5*(edges[1:]+edges[0:-1])
 data=numpy.asarray([xVals,vals]).T
 numpy.savetxt(dataFile("xVelDistribution"),data)
