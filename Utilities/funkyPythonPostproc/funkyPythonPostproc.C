b5b72a37c8dd86a8db2ff4e058e0c6d53c61c806
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2013-08-12 23:46:59 +0200

    Starting a new utility

---
+++ Utilities/funkyPythonPostproc/funkyPythonPostproc.C
@@ -0,0 +1,101 @@
+/*---------------------------------------------------------------------------*\
+ ##   ####  ######     |
+ ##  ##     ##         | Copyright: ICE Stroemungsfoschungs GmbH
+ ##  ##     ####       |
+ ##  ##     ##         | http://www.ice-sf.at
+ ##   ####  ######     |
+-------------------------------------------------------------------------------
+ =========                   |
+ \\      /   F ield          | OpenFOAM: The Open Source CFD Toolbox
+  \\    /    O peration      |
+   \\  /     A nd            | Copyright (C) 1991-2005 OpenCFD Ltd.
+    \\/      M anipulation   |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    funkyPythonPostproc
+
+Description
+
+Contributors/Copyright:
+    2013 Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
+
+ SWAK Revision: $Id$
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+#include "pythonInterpreterWrapper.H"
+
+#include "timeSelector.H"
+
+#include "IFstream.H"
+
+#include "printSwakVersion.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// Main program:
+
+int main(int argc, char *argv[])
+{
+
+    Foam::timeSelector::addOptions(false);
+    Foam::argList::validArgs.append("dictionary with specs");
+#   include "addRegionOption.H"
+    argList::validOptions.insert("interactive","");
+
+#   include "setRootCase.H"
+
+    printSwakVersion();
+
+    IFstream theFile(args.args()[1]);
+    dictionary theExpressions(theFile);
+    bool interactive(args.options().found("interactive"));
+
+    if (!args.options().found("time") && !args.options().found("latestTime")) {
+        FatalErrorIn("main()")
+            << args.executable()
+                << ": time/latestTime option is required" << endl
+            << exit(FatalError);
+    }
+
+#   include "createTime.H"
+    Foam::instantList timeDirs = Foam::timeSelector::select0(runTime, args);
+
+#   include "createNamedMesh.H"
+
+    forAll(timeDirs, timeI)
+    {
+        runTime.setTime(timeDirs[timeI], timeI);
+
+        Foam::Info << "\nTime = " << runTime.timeName() << Foam::endl;
+
+        mesh.readUpdate();
+
+
+        Info << endl;
+    }
+
+    Info << "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
