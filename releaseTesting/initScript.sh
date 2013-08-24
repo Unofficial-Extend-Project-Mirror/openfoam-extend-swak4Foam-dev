583356562ebd5849b4e879e112fa0ec8520ee7aa
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2013-08-24 21:39:14 +0200

    Now the box is provisioned to compile swak4Foam

--- releaseTesting/initScript.sh
+++ releaseTesting/initScript.sh
@@ -6,11 +6,24 @@ apt-get -y install mercurial
 apt-get -y install bison
 apt-get -y install flex
 apt-get -y install g++
+apt-get -y install make
 
 VERS=$(lsb_release -cs)
 echo deb http://www.openfoam.org/download/ubuntu $VERS main > /etc/apt/sources.list.d/openfoam.list
 
 apt-get update -y
 apt-get install -y --force-yes openfoam221
+
+SWAKDIR=/home/vagrant/swak4Foam
+
+if [ ! -e $SWAKDIR ]
+then
+    hg clone /swakSources $SWAKDIR
+    (cd $SWAKDIR; ln -s swakConfiguration.debian swakConfiguration )
+    (cd $SWAKDIR;  hg update port_2.0.x )
+    chown -R vagrant:vagrant $SWAKDIR
+else
+    echo "Repository already there. No cloning"
+fi
 
 echo "Ended"
