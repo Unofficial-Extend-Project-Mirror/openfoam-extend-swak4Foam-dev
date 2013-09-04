94be180ac1b396d1de22bc940bd7a0d88f77d98b
Bernhard Gschaider <bgschaid@ice-sf.at>
2013-09-04 16:15:29 +0200

    Moving cloning of sources and updates. Also immediately clone to the appropriate branch

--- releaseTesting/initScript.sh
+++ releaseTesting/initScript.sh
@@ -84,26 +84,6 @@ do
     apt-get install -y --force-yes "openfoam$v"
 done
 
-SWAKDIR=/home/vagrant/swak4Foam
-
-if [ ! -e $SWAKDIR ]
-then
-    echo
-    echo "Getting swak-sources"
-    echo
-    hg clone /swakSources $SWAKDIR
-    (cd $SWAKDIR; ln -s swakConfiguration.debian swakConfiguration )
-    if [ "$boxName" == "precise" ]
-    then
-	(cd $SWAKDIR;  hg update port_2.0.x )
-    else
-	(cd $SWAKDIR;  hg update develop )
-    fi
-    chown -R vagrant:vagrant $SWAKDIR
-else
-    echo "Repository already there. No cloning"
-fi
-
 echo
 echo "Copying stuff from skeleton"
 echo
@@ -118,6 +98,26 @@ do
 	cp -r "/vagrant/skel/$f" $target
     fi
 done
+
+SWAKDIR=/home/vagrant/swak4Foam
+
+if [ ! -e $SWAKDIR ]
+then
+    echo
+    echo "Getting swak-sources"
+    echo
+    if [ "$boxName" == "precise" ]
+    then
+	branchName="port_2.0.x"
+    else
+	branchName="develop"
+    fi
+    su vagrant - -c "hg clone -r $branchName /swakSources $SWAKDIR"
+    (cd $SWAKDIR; ln -s swakConfiguration.debian swakConfiguration )
+    chown -R vagrant:vagrant $SWAKDIR
+else
+    echo "Repository already there. No cloning"
+fi
 
 echo
 echo "Current ccache:"
