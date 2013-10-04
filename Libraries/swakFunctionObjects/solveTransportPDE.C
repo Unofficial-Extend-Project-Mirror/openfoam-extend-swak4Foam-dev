7ce6f3efd5114608e978ec728f2b4373a8695c7a
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2013-10-05 01:35:39 +0200

    Add relaxation to the PDE-functionObjects

--- Libraries/swakFunctionObjects/solveTransportPDE.C
+++ Libraries/swakFunctionObjects/solveTransportPDE.C
@@ -216,6 +216,10 @@ void Foam::solveTransportPDE::solve()
                 eq-=fvm::Sp(sourceImplicitField,f);
             }
 
+            if(doRelax(corr==nCorr)) {
+                eq.relax();
+            }
+
             int nNonOrthCorr=sol.lookupOrDefault<int>("nNonOrthogonalCorrectors", 0);
             for (int nonOrth=0; nonOrth<=nNonOrthCorr; nonOrth++)
             {
