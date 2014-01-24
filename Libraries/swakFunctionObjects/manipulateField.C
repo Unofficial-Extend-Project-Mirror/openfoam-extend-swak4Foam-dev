7fa7c9e10ac558ea4ed293f2068c15dc72b92e2e
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2014-01-25 00:16:20 +0100

    Update the copyright notices for the release

--- Libraries/swakFunctionObjects/manipulateField.C
+++ Libraries/swakFunctionObjects/manipulateField.C
@@ -30,6 +30,7 @@ License
 
 Contributors/Copyright:
     2010-2013 Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
+    2013 Bruno Santos <wyldckat@gmail.com>
 
  SWAK Revision: $Id:  $ 
 \*---------------------------------------------------------------------------*/
