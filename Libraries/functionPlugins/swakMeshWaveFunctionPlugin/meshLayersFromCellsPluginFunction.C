eb36115fdbd96216025f9f2f93a3681f73124c68
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2014-09-03 16:14:19 +0200

    New function to calculate layers from a set of cells

---
+++ Libraries/functionPlugins/swakMeshWaveFunctionPlugin/meshLayersFromCellsPluginFunction.C
@@ -0,0 +1,111 @@
+/*---------------------------------------------------------------------------*\
+ ##   ####  ######     |
+ ##  ##     ##         | Copyright: ICE Stroemungsfoschungs GmbH
+ ##  ##     ####       |
+ ##  ##     ##         | http://www.ice-sf.at
+ ##   ####  ######     |
+-------------------------------------------------------------------------------
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2008 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Contributors/Copyright:
+    2014 Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
+
+ SWAK Revision: $Id$
+\*---------------------------------------------------------------------------*/
+
+#include "meshLayersFromCellsPluginFunction.H"
+#include "FieldValueExpressionDriver.H"
+
+#include "addToRunTimeSelectionTable.H"
+
+namespace Foam {
+
+defineTypeNameAndDebug(meshLayersFromCellsPluginFunction,1);
+addNamedToRunTimeSelectionTable(FieldValuePluginFunction, meshLayersFromCellsPluginFunction , name, meshLayersFromCells);
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+meshLayersFromCellsPluginFunction::meshLayersFromCellsPluginFunction(
+    const FieldValueExpressionDriver &parentDriver,
+    const word &name
+):
+    meshLayersGeneralPluginFunction(
+        parentDriver,
+        name,
+        string("blockedCells internalField volLogicalField")
+    )
+{
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void meshLayersFromCellsPluginFunction::setArgument(
+    label index,
+    const string &content,
+    const CommonValueExpressionDriver &driver
+) {
+    assert(index==0);
+
+    blocked_.set(
+        new volScalarField(
+            dynamic_cast<const FieldValueExpressionDriver &>(
+                driver
+            ).getResult<volScalarField>()
+        )
+    );
+}
+
+void meshLayersFromCellsPluginFunction::initFacesAndCells()
+{
+    labelHashSet facesBlocked;
+    const cellList &cells=mesh().cells();
+
+    forAll(blocked_(),cellI) {
+        if(CommonValueExpressionDriver::toBool(blocked_()[cellI])) {
+            cellValues_[cellI]=MeshLayersDistFromPatch(0,true);
+            const cell &c=cells[cellI];
+            forAll(c,i) {
+                facesBlocked.insert(c[i]);
+            }
+        }
+    }
+    startFaces_=labelList(facesBlocked.toc());
+    forAll(startFaces_,i) {
+        faceValues_[startFaces_[i]]=MeshLayersDistFromPatch(0,true);
+    }
+    startValues_=List<MeshLayersDistFromPatch>(
+        startFaces_.size(),
+        MeshLayersDistFromPatch(1,false)
+    );
+}
+
+// * * * * * * * * * * * * * * * Friend Operators  * * * * * * * * * * * * * //
+
+} // namespace
+
+// ************************************************************************* //
