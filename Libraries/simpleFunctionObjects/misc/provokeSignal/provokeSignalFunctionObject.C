79d9015b1caca8fa4b44aa3681fcc823b8a83c70
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2014-09-21 21:20:16 +0200

    Function object that raises a signal. Used for debugging

---
+++ Libraries/simpleFunctionObjects/misc/provokeSignal/provokeSignalFunctionObject.C
@@ -0,0 +1,157 @@
+/*---------------------------------------------------------------------------*\
+ ##   ####  ######     |
+ ##  ##     ##         | Copyright: ICE Stroemungsfoschungs GmbH
+ ##  ##     ####       |
+ ##  ##     ##         | http://www.ice-sf.at
+ ##   ####  ######     |
+-------------------------------------------------------------------------------
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright  held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Contributors/Copyright:
+    2008-2011, 2013 Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
+
+ SWAK Revision: $Id$
+\*---------------------------------------------------------------------------*/
+
+#include "provokeSignalFunctionObject.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "Time.H"
+
+#include <signal.h>
+#include <unistd.h>
+
+#include "HashSet.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(provokeSignalFunctionObject, 0);
+
+    addToRunTimeSelectionTable
+    (
+        functionObject,
+        provokeSignalFunctionObject,
+        dictionary
+    );
+
+    template<>
+    const char* NamedEnum<Foam::provokeSignalFunctionObject::possibleSignals,4>::names[]=
+    {
+        "FPE",
+        "SEGV",
+        "INT",
+        "QUIT"
+    };
+    const NamedEnum<provokeSignalFunctionObject::possibleSignals,4> provokeSignalFunctionObject::possibleSignalsNames_;
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+provokeSignalFunctionObject::provokeSignalFunctionObject
+(
+    const word &name,
+    const Time& t,
+    const dictionary& dict
+)
+:
+    simpleFunctionObject(name,t,dict),
+    signalToRaise_(
+        possibleSignalsNames_[
+            word(dict.lookup("signalToRaise"))
+        ]
+    ),
+    timeToRaise_(
+        readScalar(dict.lookup("timeToRaise"))
+    ),
+    raiseOnThisProc_(false)
+{
+    if(Pstream::parRun()) {
+        HashSet<label> allProcs(
+            labelList(
+                dict.lookup("processorsToRaiseSignal")
+            )
+        );
+        raiseOnThisProc_=allProcs.found(
+            Pstream::myProcNo()
+        );
+    } else {
+        raiseOnThisProc_=true;
+    }
+
+    if(raiseOnThisProc_) {
+        Pout << endl;
+        Pout << "Will raise signal " << possibleSignalsNames_[signalToRaise_]
+            << " at time " << timeToRaise_
+            << " and there is nothing you can do about it. "
+            << "In fact the only possible use of this is testing" << endl;
+        Pout << endl;
+    }
+}
+
+void provokeSignalFunctionObject::write()
+{
+    if(
+        raiseOnThisProc_
+        &&
+        obr_.time().value()>=timeToRaise_
+    ) {
+        Pout << endl
+            << "The time has come. Raising "
+            << possibleSignalsNames_[signalToRaise_] << endl
+            << "It was nice knowing you"
+            << endl;
+
+
+        pid_t myPid=getpid();
+
+        switch(signalToRaise_) {
+            case sigFPE:
+                kill(myPid,SIGFPE);
+                break;
+            case sigSEGV:
+                kill(myPid,SIGSEGV);
+                break;
+            case sigINT:
+                kill(myPid,SIGINT);
+                break;
+            case sigQUIT:
+                kill(myPid,SIGQUIT);
+                break;
+            default:
+                FatalErrorIn("provokeSignalFunctionObject::write()")
+                    << "Unimplemented signal "
+                        << possibleSignalsNames_[signalToRaise_]
+                        << endl;
+        }
+        Pout << "We signaled (this should not be seen)" << endl;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+} // namespace Foam
+
+// ************************************************************************* //
