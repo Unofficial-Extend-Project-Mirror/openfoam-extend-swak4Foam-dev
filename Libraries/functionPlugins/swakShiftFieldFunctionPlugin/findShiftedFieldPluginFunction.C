77fd29b6b2e70398914b927f90e32f08b93d0e26
Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
2016-08-11 00:40:33 +0200

    Finding now works in parallel too. Maybe not optimal with the parallel communication

--- Libraries/functionPlugins/swakShiftFieldFunctionPlugin/findShiftedFieldPluginFunction.C
+++ Libraries/functionPlugins/swakShiftFieldFunctionPlugin/findShiftedFieldPluginFunction.C
@@ -42,6 +42,22 @@ namespace Foam {
     defineTemplateTypeNameAndDebug(findShiftedFieldScalar,0);
     addNamedToRunTimeSelectionTable(FieldValuePluginFunction, findShiftedFieldScalar , name, findShiftedScalarField);
 
+    typedef findShiftedFieldPluginFunction<vector> findShiftedFieldVector;
+    defineTemplateTypeNameAndDebug(findShiftedFieldVector,0);
+    addNamedToRunTimeSelectionTable(FieldValuePluginFunction, findShiftedFieldVector , name, findShiftedVectorField);
+
+    typedef findShiftedFieldPluginFunction<tensor> findShiftedFieldTensor;
+    defineTemplateTypeNameAndDebug(findShiftedFieldTensor,0);
+    addNamedToRunTimeSelectionTable(FieldValuePluginFunction, findShiftedFieldTensor , name, findShiftedTensorField);
+
+    typedef findShiftedFieldPluginFunction<symmTensor> findShiftedFieldSymmTensor;
+    defineTemplateTypeNameAndDebug(findShiftedFieldSymmTensor,0);
+    addNamedToRunTimeSelectionTable(FieldValuePluginFunction, findShiftedFieldSymmTensor , name, findShiftedSymmTensorField);
+
+    typedef findShiftedFieldPluginFunction<sphericalTensor> findShiftedFieldSphericalTensor;
+    defineTemplateTypeNameAndDebug(findShiftedFieldSphericalTensor,0);
+    addNamedToRunTimeSelectionTable(FieldValuePluginFunction, findShiftedFieldSphericalTensor , name, findShiftedSphericalTensorField);
+
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
@@ -68,6 +84,86 @@ findShiftedFieldPluginFunction<Type>::findShiftedFieldPluginFunction(
 
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+template<class T>
+struct FoundValue {
+    label procNo_;
+    label cell_;
+    point position_;
+    T value_;
+    bool found_;
+
+    FoundValue()
+        : procNo_(-1),
+          cell_(-1),
+          position_(vector::zero),
+          value_(pTraits<T>::zero),
+          found_(false)
+        {}
+
+    FoundValue(label c,const point &p)
+        : procNo_(Pstream::myProcNo()),
+          cell_(c),
+          position_(p),
+          value_(pTraits<T>::zero),
+          found_(false)
+        {}
+
+    void operator()(const T &v) {
+        value_=v;
+        found_=true;
+    }
+};
+
+template<class T>
+bool operator!=(const FoundValue<T> &a,const FoundValue<T> &b)
+{
+    return !(
+        a.procNo_==b.procNo_
+        &&
+        a.cell_==b.cell_
+    );
+}
+
+template<class T>
+Istream & operator>>(Istream &in,FoundValue<T> &v)
+{
+    in >> v.procNo_;
+    in >> v.cell_;
+    in >> v.position_;
+    in >> v.value_;
+    in >> v.found_;
+    return in;
+}
+
+template<class T>
+Ostream & operator<<(Ostream &out,const FoundValue<T> &v)
+{
+    out << v.procNo_ << tab;
+    out << v.cell_ << tab;
+    out << v.position_ << tab;
+    out << v.value_ << tab;
+    out << v.found_ << tab;
+    return out;
+}
+
+template<class T>
+struct KeepFound {
+    FoundValue<T> & operator()(FoundValue<T> &a,const FoundValue<T> &b) const
+        {
+            if(
+                !a.found_
+                &&
+                b.found_
+                &&
+                a.cell_==b.cell_
+                &&
+                a.procNo_==b.procNo_
+            ) {
+                a(b.value_);
+            }
+            return a;
+        }
+};
 
 template<class Type>
 void findShiftedFieldPluginFunction<Type>::doEvaluation()
@@ -78,15 +174,62 @@ void findShiftedFieldPluginFunction<Type>::doEvaluation()
         )
     );
 
+    DynamicList<FoundValue<Type> > notFound;
+
     meshSearch search(mesh());
 
     forAll(shift_(),cellI) {
         const vector &shift=shift_()[cellI];
-        const vector targetPlace=mesh().C()[cellI]+shift;
+        const point targetPlace=mesh().C()[cellI]+shift;
         label fromCell=search.findCell(targetPlace);
         if(fromCell>=0) {
             mappedField()[cellI]=field_()[fromCell];
+        } else {
+            notFound.append(
+                FoundValue<Type>(
+                    cellI,
+                    targetPlace
+                )
+            );
         }
+    }
+    notFound.shrink();
+    Pbug << notFound.size() << " cells not found" << endl;
+
+    if(Pstream::parRun) {
+        List<List<FoundValue<Type> > > data(Pstream::nProcs());
+        data[Pstream::myProcNo()]=notFound;
+        Pstream::gatherList(data);
+        Pstream::scatterList(data);
+        label cnt=0;
+        forAll(data,procI) {
+            if(procI!=Pstream::myProcNo()) {
+                forAll(data[procI],i) {
+                    FoundValue<Type> &d=data[procI][i];
+                    label fromCell=search.findCell(d.position_);
+                    if(fromCell>=0) {
+                        d(field_()[fromCell]);
+                        cnt++;
+                    }
+                }
+            }
+        }
+        Pbug << "Found " << cnt << endl;
+        forAll(data,procI) {
+            forAll(data[procI],i) {
+                Pstream::gather(data[procI][i],KeepFound<Type>());
+                Pstream::scatter(data[procI][i]);
+            }
+        }
+        cnt=0;
+        forAll(data[Pstream::myProcNo()],i) {
+            FoundValue<Type> &d=data[Pstream::myProcNo()][i];
+            if(d.found_) {
+                cnt++;
+                mappedField()[d.cell_]=d.value_;
+            }
+        }
+        Pbug << "Found " << cnt << " of " << notFound.size() << endl;
     }
 
     mappedField->correctBoundaryConditions();
