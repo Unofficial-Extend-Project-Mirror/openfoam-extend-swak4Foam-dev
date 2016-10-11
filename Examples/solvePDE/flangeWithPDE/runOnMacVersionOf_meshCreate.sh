14f30ac29d3b6f361f2e08984df2bdc44169b137
Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
2016-10-11 11:44:12 +0200

    Additional example cases supported by OF 4.0

--- Examples/solvePDE/flangeWithPDE/runOnMacVersionOf_meshCreate.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# Necessary because Mac OS X does not pass this variable
-export LD_LIBRARY_PATH=/Users/bgschaid/OpenFOAM/OpenFOAM-v1606+/platforms/darwinIntel64Clang37DPInt32Opt/lib/openmpi-macport-mp:/Users/bgschaid/OpenFOAM/OpenFOAM-v1606+/platforms/darwinIntel64Clang37DPInt32Opt/lib:/Users/bgschaid/OpenFOAM/bgschaid-v1606+/platforms/darwinIntel64Clang37DPInt32Opt/lib:/Users/bgschaid/OpenFOAM/site/v1606+/platforms/darwinIntel64Clang37DPInt32Opt/lib
-
-
-ansysToFoam flange.ans -scale 0.001
