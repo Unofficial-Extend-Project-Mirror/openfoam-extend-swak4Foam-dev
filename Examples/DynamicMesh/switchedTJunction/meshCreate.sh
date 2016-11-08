47f1cf7cf77c07d4c58f85667d364724a0d8dca7
Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
2016-11-08 22:44:21 +0100

    Now the outlets bend down. Still reflections

--- Examples/DynamicMesh/switchedTJunction/meshCreate.sh
+++ Examples/DynamicMesh/switchedTJunction/meshCreate.sh
@@ -10,3 +10,8 @@ topoSet -constant
 
 # split the mesh to generate the ACMI coupled patches
 createBaffles -overwrite
+
+mkdir 0
+cp 0.org/valveField 0
+funkyWarpMesh -dictExt outletDown -allowFunctionObjects -overwrite
+rm -r 0
