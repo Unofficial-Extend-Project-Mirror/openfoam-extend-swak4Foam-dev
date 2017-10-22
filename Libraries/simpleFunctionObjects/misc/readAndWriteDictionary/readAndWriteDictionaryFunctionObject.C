f44682e377be2b82960b64a3bda09f1e9caf72fd
Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
2017-10-22 20:37:50 +0200

    Add function object to read dictionaries and automatically write them

---
+++ Libraries/simpleFunctionObjects/misc/readAndWriteDictionary/readAndWriteDictionaryFunctionObject.C
@@ -0,0 +1,163 @@
+/*---------------------------------------------------------------------------*\
+ ##   ####  ######     |
+ ##  ##     ##         | Copyright: ICE Stroemungsfoschungs GmbH
+ ##  ##     ####       |
+ ##  ##     ##         | http://www.ice-sf.at
+ ##   ####  ######     |
+-------------------------------------------------------------------------------
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright  held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is based on OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Contributors/Copyright:
+    2017 Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
+
+ SWAK Revision: $Id$
+\*---------------------------------------------------------------------------*/
+
+#include "readAndWriteDictionaryFunctionObject.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "polyMesh.H"
+#include "IOmanip.H"
+#include "swakTime.H"
+
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(readAndWriteDictionaryFunctionObject, 0);
+
+    addToRunTimeSelectionTable
+    (
+        functionObject,
+        readAndWriteDictionaryFunctionObject,
+        dictionary
+    );
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+readAndWriteDictionaryFunctionObject::readAndWriteDictionaryFunctionObject
+(
+    const word &name,
+    const Time& t,
+    const dictionary& dict
+)
+:
+    simpleFunctionObject(name,t,dict),
+    inConstant_(
+        readBool(
+            dict.lookup("inConstant")
+        )
+    ),
+    inSystem_(
+        readBool(
+            dict.lookup("inSystem")
+        )
+    ),
+    mustExist_(
+        (inConstant_ || inSystem_)
+        ?
+        true
+        :
+        readBool(
+            dict.lookup("mustExist")
+        )
+    ),
+    autowrite_(
+        (inConstant_ || inSystem_)
+        ?
+        false
+        :
+        readBool(
+            dict.lookup("autowrite")
+        )
+    ),
+    dictName_(
+        dict.lookup("dictName")
+    ),
+    localName_(
+        dict.lookupOrDefault<word>(
+            "localName",
+            (inConstant_ || inSystem_)
+            ? ""
+            : "swak4Foam"
+        )
+    )
+{
+    if(inConstant_ && inSystem_) {
+        FatalErrorIn("readAndWriteDictionaryFunctionObject::readAndWriteDictionaryFunctionObject")
+            << "Options 'inSystem' and 'inConstant' both set in " << dict.name()
+                << " although they are mutual exclusive"
+                << endl
+                << exit(FatalError);
+    }
+    if(!dict.found("localName")) {
+        WarningIn("readAndWriteDictionaryFunctionObject::readAndWriteDictionaryFunctionObject")
+            << "In " << dict.name() << " no entry 'localName' defined. "
+                << "Assuming 'swak4Foam'"
+                << endl;
+    }
+
+    dict_.set(
+        new IOdictionary(
+            IOobject(
+                dictName_,
+                (
+                    inConstant_
+                    ? "constant"
+                    : (
+                        inSystem_
+                        ? "system"
+                        :obr_.time().timeName()
+                    )
+                ),
+                localName_,
+                obr_,
+                mustExist_ ? IOobject::MUST_READ : IOobject::READ_IF_PRESENT,
+                autowrite_ ? IOobject::AUTO_WRITE : IOobject::NO_WRITE
+            )
+        )
+    );
+
+    Info << "Read dictionary " << dict_->path() << endl;
+
+#ifdef FOAM_FUNCTIONOBJECT_HAS_SEPARATE_WRITE_METHOD_AND_NO_START
+    this->start();
+#endif
+}
+
+bool readAndWriteDictionaryFunctionObject::start()
+{
+    return simpleFunctionObject::start();
+}
+
+void readAndWriteDictionaryFunctionObject::writeSimple()
+{
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+} // namespace Foam
+
+// ************************************************************************* //
