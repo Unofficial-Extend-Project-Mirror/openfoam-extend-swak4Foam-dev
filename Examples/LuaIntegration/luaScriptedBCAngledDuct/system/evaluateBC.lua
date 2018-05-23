4c7fc3fcc9c45792e6592cbb8ddcd9267c9f52f9
Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
2018-05-24 01:07:06 +0200

    Better usage: state machine for every face

--- Examples/LuaIntegration/luaScriptedBCAngledDuct/system/evaluateBC.lua
+++ Examples/LuaIntegration/luaScriptedBCAngledDuct/system/evaluateBC.lua
@@ -10,6 +10,7 @@ if type(TInner)=="number" then
 end
 
 -- Don't think too much about this. It is just for testing
+--[[
 for i=1,#referenceT do
    if position[i].z>0 then
       refValue[i]=referenceT[i]+5
@@ -23,4 +24,49 @@ for i=1,#referenceT do
          valueFraction[i]=1
       end
    end
+end
+--]]
+
+maxVel=1e-15
+for i=1,#slipVel do
+   if slipVel[i]>maxVel then
+      maxVel=slipVel[i]
+   end
+end
+print("Maximum slip velocity "..maxVel)
+
+cnt={0,0,0}
+
+for i=1,#stateFace do
+   state=stateFace[i]
+   cnt[state]=cnt[state]+1
+
+   if state==1 then
+      refValue[i]=referenceT[i]+5
+      valueFraction[i]=0.5
+      if TInner[i]-referenceT[i]>1 then
+         stateFace[i]=2
+      end
+   elseif state==2 then
+      refValue[i]=referenceT[i]-5*slipVel[i]/maxVel
+      valueFraction[i]=1
+      if TInner[i]-referenceT[i]<-1 then
+         stateFace[i]=3
+      end
+   elseif state==3 then
+      valueFraction[i]=0
+      refValue[i]=referenceT[i]
+      if position[i].z<0 then
+         refGrad[i]=-10
+      else
+         refGrad[i]=10
+      end
+   else
+      -- Should not happen
+      stateFace[i]=0
+      valueFraction[i]=0
+   end
+end
+for i,nr in pairs(cnt) do
+   print("State "..i.." : "..nr)
 end
--- Examples/LuaIntegration/luaScriptedBCAngledDuct/system/initBC.lua
+++ Examples/LuaIntegration/luaScriptedBCAngledDuct/system/initBC.lua
@@ -4,3 +4,8 @@ referenceT=swak4foam.newScalarField(#refValue)
 for i=1,#referenceT do
    referenceT[i]=refValue[i]
 end
+
+stateFace=swak4foam.newScalarField(#refValue)
+for i=1,#referenceT do
+   stateFace[i]=1
+end
