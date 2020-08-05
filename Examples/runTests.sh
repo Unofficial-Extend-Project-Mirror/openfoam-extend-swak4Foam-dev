83a3bf893621f554dcf9f8379035018fceb38ddc
Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
2020-08-05 13:01:35 +0200

    Now FOAM_SIGFPE should be correctly propagated in different Distros

--- Examples/runTests.sh
+++ Examples/runTests.sh
@@ -4,6 +4,8 @@ topDir=$(pwd)
 marker="testStarted"
 
 function runIt {
+    local do_fpe=$1
+    shift 1
     local here=$1
     shift 1
     cd $here
@@ -13,6 +15,13 @@ function runIt {
         return
     fi
     touch $marker
+    if [ "$do_fpe" == "0" ]; then
+        echo "Unsetting FOAM_SIGFPE"
+        unset FOAM_SIGFPE
+    else
+        echo "Setting FOAM_SIGFPE to $do_fpe"
+        export FOAM_SIGFPE=$do_fpe
+    fi
     echo
     echo "$here starting"
     echo
@@ -26,61 +35,79 @@ function runIt {
 
 export FOAM_ABORT=1
 
-runIt DynamicMesh/hecticInletACMI2D
-runIt DynamicMesh/rotatingTank2D  --run-until=5
-runIt DynamicMesh/switchedInletACMI2D
-runIt DynamicMesh/switchedTJunction --run-until=0.5
-runIt FiniteArea/planeTransport
-runIt functionPlugins/chemPluginCounterFlowFlame2D
-runIt functionPlugins/shiftCavity
-FOAM_SIGFPE=false runIt FvOptions/angleDuctWithSources
-runIt FvOptions/heatExchangerSources
-runIt groovyBC/average-t-junction
-runIt groovyBC/circulatingSplash --run-until=0.004
-runIt groovyBC/delayed-t-junction
-FOAM_SIGFPE=false runIt groovyBC/fillingTheDam
-runIt groovyBC/jumpChannel
-runIt groovyBC/movingConeDistorted
-FOAM_SIGFPE=false runIt groovyBC/pulsedPitzDaily --run-until=1e-3
-runIt groovyBC/wobbler
-runIt ImmersedBC/pitzDailyImmersed
-runIt Lagrangian/functionObjects/angledDuctWithBalls
-runIt Lagrangian/functionObjects/hotStream
-runIt Lagrangian/functionObjects/icoFoamCavityWithParcel
-runIt Lagrangian/LanguageIntegration/angledDuctWithLuaInject
-FOAM_SIGFPE=false runIt Lagrangian/LanguageIntegration/angledDuctWithPython2Inject
-runIt Lagrangian/LanguageIntegration/angledDuctWithPython3Inject
-runIt Lagrangian/parser/parcelInBoxWithExpressions
-runIt Lagrangian/parser/simplifiedSiwek
-runIt LuaIntegration/flowStatePitzDaily
-runIt LuaIntegration/luaScriptedBCAngledDuct
-runIt LuaIntegration/raiseRelaxationPitzDaily
-runIt MakeAxialMesh/axialCavity
-runIt MakeAxialMesh/axialPitzDaily
-runIt manipulateFvSolutionFvSchemes/pitzDailyStateMachineSwitched
-runIt manipulateFvSolutionFvSchemes/pitzDailyTimeSwitched
-FOAM_SIGFPE=false runIt other/angledDuctImplicit
-runIt other/capillaryRise --run-until=0.05
-runIt other/counterFlowFlame2DInitialized
-runIt other/simpleBendedPipe
-runIt other/topoSetDamBreak
-runIt Python3Integration/flowStatePitzDaily
-runIt Python3Integration/python3ScriptedBCAngledDuct
-runIt runTimeCondition/simpleSwakCar
-runIt SimpleSurface/littleVolcano --run-until=0.1
-runIt solvePDE/flangeWithPDE
-runIt solvePDE/pitzDailyWithPDE
-runIt StateMachine/stateCavity
-FOAM_SIGFPE=false runIt tests/AMIPipe --run-until=0.02
-runIt tests/delayedMappingChannels
-runIt tests/languageIntegration/luaIntegration
-FOAM_SIGFPE=false runIt tests/languageIntegration/pythonIntegration
-FOAM_SIGFPE=false runIt tests/languageIntegration/python3Integration
-runIt tests/mappingChannels
-runIt tests/mappingChannelsNonUniform
-runIt tests/SearchableSurfaces/objectTests
-runIt tests/SearchableSurfaces/operationsTest
-runIt tests/SearchableSurfaces/operationsTestRotated
-runIt tests/SearchableSurfaces/scaledIglooWithFridges
-runIt tests/swakDataEntry/flowRateAngledDuct
-runIt tests/testFOExecution
+if [ -n "$FOAM_API" ]; then
+    switch_off_FPE=false
+    switch_on_FPE=true
+else
+    switch_off_FPE=0
+    switch_on_FPE=1
+fi
+
+# stored_FPE=$FOAM_SIGFPE
+# unset FOAM_SIGFPE
+
+# switch_off_FPE="FOAM_SIGFPE=false"
+# switch_off_FPE="FOAM_SIGFPE=0"
+# switch_off_FPE=
+
+# switch_on_FPE=
+# export FOAM_SIGFPE=true
+
+runIt $switch_on_FPE DynamicMesh/hecticInletACMI2D
+runIt $switch_on_FPE DynamicMesh/rotatingTank2D  --run-until=5
+runIt $switch_on_FPE DynamicMesh/switchedInletACMI2D
+runIt $switch_on_FPE DynamicMesh/switchedTJunction --run-until=0.5
+runIt $switch_on_FPE FiniteArea/planeTransport
+runIt $switch_on_FPE functionPlugins/chemPluginCounterFlowFlame2D
+runIt $switch_on_FPE functionPlugins/shiftCavity
+runIt $switch_off_FPE FvOptions/angleDuctWithSources
+runIt $switch_on_FPE FvOptions/heatExchangerSources
+runIt $switch_on_FPE groovyBC/average-t-junction
+runIt $switch_on_FPE groovyBC/circulatingSplash --run-until=0.004
+runIt $switch_on_FPE groovyBC/delayed-t-junction
+runIt $switch_off_FPE groovyBC/fillingTheDam
+runIt $switch_on_FPE groovyBC/jumpChannel
+runIt $switch_on_FPE groovyBC/movingConeDistorted
+runIt $switch_off_FPE groovyBC/pulsedPitzDaily --run-until=1e-3
+runIt $switch_on_FPE groovyBC/wobbler
+runIt $switch_on_FPE ImmersedBC/pitzDailyImmersed
+runIt $switch_on_FPE Lagrangian/functionObjects/angledDuctWithBalls
+runIt $switch_on_FPE Lagrangian/functionObjects/hotStream
+runIt $switch_on_FPE Lagrangian/functionObjects/icoFoamCavityWithParcel
+runIt $switch_on_FPE Lagrangian/LanguageIntegration/angledDuctWithLuaInject
+runIt $switch_off_FPE Lagrangian/LanguageIntegration/angledDuctWithPython2Inject
+runIt $switch_on_FPE Lagrangian/LanguageIntegration/angledDuctWithPython3Inject
+runIt $switch_on_FPE Lagrangian/parser/parcelInBoxWithExpressions
+runIt $switch_on_FPE Lagrangian/parser/simplifiedSiwek
+runIt $switch_on_FPE LuaIntegration/flowStatePitzDaily
+runIt $switch_on_FPE LuaIntegration/luaScriptedBCAngledDuct
+runIt $switch_on_FPE LuaIntegration/raiseRelaxationPitzDaily
+runIt $switch_on_FPE MakeAxialMesh/axialCavity
+runIt $switch_on_FPE MakeAxialMesh/axialPitzDaily
+runIt $switch_on_FPE manipulateFvSolutionFvSchemes/pitzDailyStateMachineSwitched
+runIt $switch_on_FPE manipulateFvSolutionFvSchemes/pitzDailyTimeSwitched
+runIt $switch_off_FPE other/angledDuctImplicit
+runIt $switch_on_FPE other/capillaryRise --run-until=0.05
+runIt $switch_on_FPE other/counterFlowFlame2DInitialized
+runIt $switch_on_FPE other/simpleBendedPipe
+runIt $switch_on_FPE other/topoSetDamBreak
+runIt $switch_on_FPE Python3Integration/flowStatePitzDaily
+runIt $switch_on_FPE Python3Integration/python3ScriptedBCAngledDuct
+runIt $switch_on_FPE runTimeCondition/simpleSwakCar
+runIt $switch_on_FPE SimpleSurface/littleVolcano --run-until=0.1
+runIt $switch_on_FPE solvePDE/flangeWithPDE
+runIt $switch_on_FPE solvePDE/pitzDailyWithPDE
+runIt $switch_on_FPE StateMachine/stateCavity
+runIt $switch_off_FPE tests/AMIPipe --run-until=0.02
+runIt $switch_on_FPE tests/delayedMappingChannels
+runIt $switch_on_FPE tests/languageIntegration/luaIntegration
+runIt $switch_off_FPE tests/languageIntegration/pythonIntegration
+runIt $switch_off_FPE tests/languageIntegration/python3Integration
+runIt $switch_on_FPE tests/mappingChannels
+runIt $switch_on_FPE tests/mappingChannelsNonUniform
+runIt $switch_on_FPE tests/SearchableSurfaces/objectTests
+runIt $switch_on_FPE tests/SearchableSurfaces/operationsTest
+runIt $switch_on_FPE tests/SearchableSurfaces/operationsTestRotated
+runIt $switch_on_FPE tests/SearchableSurfaces/scaledIglooWithFridges
+runIt $switch_on_FPE tests/swakDataEntry/flowRateAngledDuct
+runIt $switch_on_FPE tests/testFOExecution
